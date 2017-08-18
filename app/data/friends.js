f3d5ca14c1f3ef93b18dcbffd8c25e0bc0085c1c
Shawn Mukherji <Shawn@Shawns-Mac.local>
2017-08-17 20:21:22 -0500

    friends.js dummy array

---
+++ app/data/friends.js
@@ -0,0 +1,43 @@
+// =============== Friends Data ==============//
+
+var friends = [
+    {
+        "name": "Shawn",
+        "photo": "https://wwww.imgur.com",
+        "scores":[
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            1
+            ]
+        },
+    {
+        "name": "Wes",
+        "photo": "https://www.imgur.com",
+        "scores":[
+            1,
+            3,
+            5,
+            6,
+            1,
+            3,
+            5,
+            7,
+            9,
+            3,
+            6
+        ]
+
+    }
+    
+
+
+]
+
+module.exports = friends;
